9e30d56faedb1afa83986c7c08d27111282f9152
Orxan <ibrahimzada003@gmail.com>
2023-10-28 14:49:00 +0400

    Add project files.

---
+++ salam123456789/Program.cs
@@ -0,0 +1,150 @@
+﻿#region task1
+
+//Console.WriteLine(" 4 reqemli ededi daxil et");
+//int num1=int.Parse(Console.ReadLine());
+//string streem = num1 .ToString();
+//while (true)
+//{
+//    if (streem.Length == 4)
+//    {
+//        Console.WriteLine("7"+streem+"8");
+//        break;
+//    }
+//    else
+//    {
+//        Console.WriteLine("duzgun daxil et ");
+//        streem = Console.ReadLine();
+//    }
+//}
+#endregion
+#region task2
+//Console.WriteLine("eded daxil edin");
+//int num2=int.Parse(Console.ReadLine());
+//string number=num2.ToString();
+//while (true)
+//{
+//    if (number.Length == 3) 
+//    {
+//        Console.WriteLine(number+number);
+//        break;
+//    }
+
+//    else 
+//    {
+//        Console.WriteLine("duzgun ededi daxil edin");
+//        number = Console.ReadLine();
+//    }
+//}
+
+#endregion
+#region task3
+//Console.WriteLine("ededi daxil edin");
+//int num3=int.Parse(Console.ReadLine());
+//string number = num3.ToString();
+//while (true)
+//{
+//    if (number.Length == 5)
+//    {
+//        double result = (num3 * 0.18) * 0.03;
+//        Console.WriteLine(result);
+//        break;
+//    }
+//    else
+//    {
+//        Console.WriteLine("duzgun eded daxil et");
+//        number=Console.ReadLine();
+//}
+//}
+#endregion
+#region task4
+//Console.WriteLine("ededi daxil edin");
+//int num4=int.Parse(Console.ReadLine());
+//string number=num4.ToString();
+//while (true) 
+//{ 
+//if (number.Length ==3) 
+//    {
+//        int numa = (num4 * 10) + 7;
+//        double result = numa*0.07;
+//        Console.WriteLine(result);
+//        break;
+//    }
+//    else 
+//    {
+//        Console.WriteLine("duzgun eded daxil et");
+//        number = Console.ReadLine();
+//    }
+//}
+#endregion
+#region task5
+//Console.WriteLine("Xahis edirem ededi daxil edin");
+//int num_4 = int.Parse(Console.ReadLine());
+//string stream = num_4.ToString();
+
+
+//while (true)
+//{
+//    if (stream.Length == 4)
+//    {
+//        string result = "4" + stream + "44";
+//        int result1 = int.Parse(result);
+//        int lastresult = (result1 * 44) / 100;
+//        Console.WriteLine(lastresult);
+//        break;
+//    }
+//    else
+//    {
+//        Console.WriteLine("terar daxil et");
+//        stream = Console.ReadLine();
+//    }
+//}
+#endregion
+#region task6
+//Console.WriteLine("xahis edirem ededi daxil edin");
+//int num_4 = int.Parse(Console.ReadLine());
+//string stream = num_4.ToString();
+//while (true)
+//{
+//    if (stream.Length == 4)
+//    {
+//        int resultfirst = (num_4 * 20) / 100;
+//        int resultsecond = (resultfirst * 10) / 100;
+//        int resultthird = (resultsecond * resultsecond);
+//        Console.WriteLine(resultthird);
+//        break;
+//    }
+//    else
+//    {
+//        Console.WriteLine("duzgun alqoritmi qur");
+//        stream = Console.ReadLine();
+
+//    }
+//}
+#endregion
+#region Task7
+//Console.WriteLine("Xahis edirem a ni daxil edin");
+//int num_a = int.Parse(Console.ReadLine());
+//Console.WriteLine("Xahis b ni ededi daxil edin");
+//int num_b = int.Parse(Console.ReadLine());
+//string str_a = num_a.ToString();
+//string str_b = num_b.ToString();
+//while (true)
+//{
+//    if (str_a.Length == 5 && str_b.Length == 5)
+//    {
+//        int firstPart = num_a + num_b;
+//        string firstPartString = firstPart.ToString();
+//        string result1 = "5" + firstPartString + "5";
+//        int resultPart2 = int.Parse(result1);
+//        int lastResult = (resultPart2 * 5) / 100;
+//        Console.WriteLine(lastResult);
+//        break;
+//    }
+//    else
+//    {
+//        Console.WriteLine("Duzgun formatda eded daxil etmediniz.");
+//        str_a = Console.ReadLine();
+//        str_b  = Console.ReadLine();
+//    }
+//}
+#endregion
